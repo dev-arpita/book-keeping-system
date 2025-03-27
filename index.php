41425b8ec0199f52984adbaf50eba22194abb86a
Arpita Roy <arpita.roy.workar@gmail.com>
2025-03-27 18:03:35 +0530

    database connection started but got some issue on database table

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
  require_once 'functions.php';
  require_once 'routes.php';
+
+ //connect with the database
+ // PDO class
+
+ //dsn- connection string that connects to the dtabase 
+ $dsn= 'mysql;host:localhost;port:3306;dbname:myapp;charset:utf8mb4';
+$pdo = new PDO($dsn);
