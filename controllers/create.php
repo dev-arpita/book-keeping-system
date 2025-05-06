a3f46c26acb58ddf0c9e20aac7edfb1966222e5e
Arpita Roy <arpita.roy.workar@gmail.com>
2025-05-06 19:57:32 +0530

    "" logic is not working

--- controllers/create.php
+++ controllers/create.php
@@ -4,12 +4,21 @@ $heading = 'Create New';
 $config = require_once 'config.php';
 $db = new Database($config['database']);
 
-if($_SERVER["REQUEST_METHOD"] === 'POST'){
-    $db->query('INSERT INTO books( names, category, user_id ) VALUES(:names, :category, :user_id)', [
-        'names' => $_POST['names'],
-        'category' => $_POST['category'],
-        'user_id' => 1,
-    ]);
-}
 
+if($_SERVER["REQUEST_METHOD"] === 'POST'){
+    $errors = [];
+    
+    if(strlen($_POST['names'] === 0)){
+        $errors['names'] = "The name is required";
+        $errors['category'] = "The category is required";
+
+    }
+    if(empty($errors)){
+        $db->query('INSERT INTO books( names, category, user_id ) VALUES(:names, :category, :user_id)', [
+            'names' => $_POST['names'],
+            'category' => $_POST['category'],
+            'user_id' => 1,
+        ]);
+    }
+}
 require_once "./views/create.view.php";
--- views/create.view.php
+++ views/create.view.php
@@ -4,13 +4,26 @@
 <main>
 <div class="ml-16 pt-8 ">
  <form method="POST" class="flex flex-col max-w-lg gap-6">
+
     <label for="names">Book Name</label>
-    <input type="text" name="names" id="names" >
+    <input type="text" name="names" id="names" class="px-2 py-1">
+    <?php if(isset($errors['names'])):?>
+      <p class="text-red-500 text-xs">
+        <?= $errors['names'];?>
+      </p>
+    <?php endif;?>
+
     <label for="category">Book category</label>
-    <input type="text" name="category" id="category">
+    <input type="text" name="category" id="category" class="px-2 py-1">
+    <?php if(isset($errors['category'])):?>
+      <p class="text-red-500 text-xs">
+        <?= $errors['category']?>
+      </p>
+    <?php endif;?>
+
     <button type="submit" class="border-2 border-blue-800 text-blue-800 font-bold w-[14em] rounded">
       <a href="/books/create">Save</a>
     </button>
  </form>
 </main>
-<?php require_once './views/partials/footer.php'; ?>
+<?php require_once './views/partials/footer.php';?>
